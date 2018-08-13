c740d4528d6936226d29b8329defe92010959edf
Guillaume <jeusel.guillaume@gmail.com>
2018-08-13 22:16:53 +0200

    sklearn crossval ok

--- src/homeserv_inter/model.py
+++ src/homeserv_inter/model.py
@@ -1,4 +1,6 @@
 import warnings
+from hyperopt import hp, tpe
+from hyperopt.fmin import fmin
 
 import lightgbm as lgb
 from sklearn import metrics, model_selection
@@ -26,6 +28,21 @@ class LgbHomeService(HomeServiceDataHandle):
         "nthreads": 4,
     }
 
+    hyperopt_space = {
+        'n_estimators': hp.quniform('n_estimators', 25, 500, 25),
+        'max_depth': hp.quniform('max_depth', 1, 10, 1)
+    }
+
+    # Used by RandomizedSearchCV
+    params_discovery = {
+        "learning_rate": [0.08, 0.1, 0.15],
+        "max_depth": [20, 25, 30],
+        "num_leaves": [1000, 1200, 1400],
+        "n_estimators": [100, 200, 300],
+        # 'min_data_in_leaf': [800],
+    }
+
+
     def validate(self, early_stopping_rounds=20):
         dtrain, dtest = self.get_train_valid_set(as_lgb_dataset=True)
         return lgb.train(
@@ -43,18 +60,8 @@ class LgbHomeService(HomeServiceDataHandle):
             early_stopping_rounds=early_stopping_rounds,
         )
 
-
-class HomeService(HomeServiceDataHandle):
-    def best_params_discovery(self):
-        df, label_encoders = self._get_formatted_datas()
-
-        param_dist = {
-            "learning_rate": [0.08, 0.1, 0.15],
-            "max_depth": [20, 25, 30],
-            "num_leaves": [1000, 1200, 1400],
-            "n_estimators": [100, 200, 300],
-            # 'min_data_in_leaf': [800],
-        }
+    def params_tuning_sklearn(self):
+        Xtrain, ytrain = self.get_train_set(as_lgb_dataset=False)  # False here !
 
         model = lgb.LGBMClassifier(
             silent=False, objective="binary", n_jobs=4, random_state=42, verbose=-1
@@ -64,15 +71,17 @@ class HomeService(HomeServiceDataHandle):
         with Timer("Randomized Search Cross Validation", True):
             rand_grid_search = model_selection.RandomizedSearchCV(
                 model,
-                param_distributions=param_dist,
-                n_iter=30,
+                param_distributions=self.params_discovery,
+                n_iter=50,
                 scoring=["roc_auc", "accuracy"],
                 refit="roc_auc",
                 cv=5,
                 verbose=2,
             )
-            rand_grid_search.fit(
-                df.drop(columns=["target"]), df[["target"]].values.ravel()
-            )
+            rand_grid_search.fit(Xtrain, ytrain.values.ravel())
 
+        print('Best Params: {}'.format(rand_grid_search.best_params_))
         return rand_grid_search
+
+    def params_tuning_hyperopt(self):
+        pass
