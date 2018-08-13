ecd5412286aad5f14dc5d794d159baed1113a0c0
Guillaume <jeusel.guillaume@gmail.com>
2018-08-13 18:58:49 +0200

    Floyd OK but run out of memory
    Free CPU, need to pay ...

--- floyd_deps_installer.sh
+++ floyd_deps_installer.sh
@@ -4,6 +4,9 @@ env  # print out env
 echo "Running ls"
 ls
 
+# Fix pbr (.git not clonned during floyd job)
+git init . && git add . && git commit -m "floyd commit"
+
 pip install -U pip
 pip install -r requirements.txt
 pip install --editable .
@@ -12,10 +15,10 @@ pip install --editable .
 pkg_dir=$PWD
 ln -s /data .
 
-## install ml_utils
-#git clone https://github.com/gjeusel/ml_utils
-#(cd ml_utils && pip install -e .)
+# install ml_utils
+git clone https://github.com/gjeusel/ml_utils
+(cd ml_utils && pip install -e .)
 
-## install wax-toolbox
-#git clone https://github.com/gjeusel/wax-toolbox
-#(cd wax-toolbox && pip install -e .)
+# install wax-toolbox
+git clone https://github.com/gjeusel/wax-toolbox
+(cd wax-toolbox && pip install -e .)
